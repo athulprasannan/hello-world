5da0599147b916991c7c0fabf56a7747e8c496c9
Athul Prasannan <athulprasannan5@gmail.com>
2016-06-06 15:05:19 +0530

    Create pgm1.cpp

---
+++ pgm1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int  main(void) {
+    long long int a;
+ cin>>a;
+ if(a==0)
+ cout<<"number is zero";
+ else if(a>0)
+ cout<<"number is positive";
+ else if(a<0)
+ cout<<"number is negative";
+
+}
