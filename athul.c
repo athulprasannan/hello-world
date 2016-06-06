a0d8bcc5c5cf8e90c52a582a63b5a738c3dcde98
Athul Prasannan <athulprasannan5@gmail.com>
2016-06-06 14:52:12 +0530

    update athul.c

--- athul.c
+++ athul.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
-void main()
+int main()
 {
  long long int a;
- scand("%lld",&a);
+ scanf("%lld",&a);
  if(a==0)
  printf("number is zero");
  else if(a>0)
  printf("number is positive");
  else if(a<0)
  printf("number is negative");
+ return 0;
 } 
